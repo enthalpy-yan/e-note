a3e00f11daf19e2f884f2a0696f0528b6c664a9a
enthalpy.yan <enthalpy.yan311@gmail.com>
2013-09-12 00:31:14 -0400

    Refactoring front-end

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,119 +6,32 @@ angular.module('myApp.controllers', []).
   controller('AppCtrl', function ($scope, socket) {
 
   }).
-  controller('MyCtrl1', function ($scope, $timeout, $http, socket, $location) {
-    //global variable
-    var PAGELIMIT = 9;
-    $scope.flatUiColors = {
-      "c1" : "#1abc9c",
-      "c2" : "#2ecc71",
-      "c3" : "#2980b9",
-      "c4" : "#8e44ad",
-      "c5" : "#34495e",
-      "c6" : "#d35400",
-      "c7" : "#c0392b",
-      "c8" : "#7f8c8d",
-      "c9" : "#f39c12",
-      "c0" : "#3498db"
+  controller('MyCtrl1', function ($scope, $timeout, $http, socket, $location, Note) {
+    var PAGELIMIT = 18;
+
+    $scope.note = new Note();
+
+    $scope.loadNotes = function(limit) {
+      $scope.note.getNotes(limit);
     }
 
-    $scope.testNotes = [
-      {"sentence":"1"},
-      {"sentence":"2"},      
-      {"sentence":"3"}
-    ]
-
-    $scope.limit = PAGELIMIT;
-    $scope.notes = [];
-    $scope.endOfLine = false;
-    $scope.notesLength = 0;
-    $scope.busy = false;
-    $scope.bottomShadow = {    
-      "box-shadow": "0 0px 0px 0px #000000",
-      "-moz-box-shadow": "0 0px 0px 0px #000000",
-      "-webkit-box-shadow": "0 0px 0px 0px #000000"
+    $scope.nextPage = function(limit) {
+      $scope.loadNotes(limit);
+      PAGELIMIT += 9;
     }
 
-    $(document).on("scroll",function(){
-      if($(document).scrollTop()>10){
-          $scope.bottomShadow = {    
-            "-webkit-box-shadow": "0px 2px 4px #B8B8B8",
-               "-moz-box-shadow": "0px 2px 4px #B8B8B8",
-                    "box-shadow": "0px 2px 4px #B8B8B8"
-          };
-      } else{
-          $scope.bottomShadow = {    
-            "box-shadow": "0 0px 0px 0px #000000",
-            "-moz-box-shadow": "0 0px 0px 0px #000000",
-            "-webkit-box-shadow": "0 0px 0px 0px #000000"
-          };
-      }
-    });
-
-    $scope.getNotesLength = function() {
-      $http.get('/api/notes/count').success(function(data) {
-        $scope.notesLength = data;
-        $http.get('/api/notes?limit=9').success(function(data) {
-          $scope.notes = data;
-          $scope.busy = false;
-        });
-      });
-    }
-
+    //Socket IO event handler
     socket.on('note: added', function (data) {
-      $http.get('/api/notes').success(function(data) {
-        $scope.notes.unshift(data[0]);
-      });
+
     });
 
     socket.on('note: updated', function (data) {
-      $scope.loadMore($scope.notes.length);
+
     });
 
     socket.on('note: removed', function (data) {
-      $scope.loadMore($scope.notes.length - 1);
+
     });
-
-    $scope.loadMore = function(limit) {
-      if ($scope.busy) return;
-      $scope.busy = true;
-      $http.get('/api/notes?limit=' + limit).success(function(data) {
-        $scope.notes = data;
-        $scope.limit += PAGELIMIT;
-        $scope.busy = false;
-      });
-    };
-
-    $scope.clickFunction = function() {
-      $location.path("/view2");
-    }
-
-    $scope.fadeInClass = function() {
-      return {
-        "opacity" : "1"
-      };
-    }
-
-    $scope.showIcon = function(index) {
-      $scope.notes[index].show = true;
-    }
-
-    $scope.hideIcon = function(index) {
-      $scope.notes[index].show = false;
-    }
-
-    $scope.deleteNote = function(index) {
-      $http.delete('/api/notes/' + $scope.notes[index]['_id']).success(function(data) {
-
-      })
-    }
-
-    $scope.barColor = function(index) {
-      var colorNum = parseInt(index - 1)
-      return {
-        "background-color" : $scope.flatUiColors['c' + colorNum]
-      };
-    }
 
   }).
   controller('navbarController', function ($scope, $location) {
--- public/js/services.js
+++ public/js/services.js
@@ -6,4 +6,26 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+	factory('Note', function($http) {
+	  var Note = function() {
+	    this.notes = [];
+	    this.busy = false;
+	  };
+
+	  /*
+		 * Load note data
+		 * @param limit: how many notes you want to load each time.
+		 *
+	   */
+	  Note.prototype.getNotes = function(limit) {
+	    if (this.busy) return;
+	    this.busy = true;
+	    $http.get('/api/notes?limit=' + limit).success(function(data) {
+        this.notes = data;
+        this.busy = false;
+      }.bind(this));
+	  };
+
+	  return Note;
+
+	});
